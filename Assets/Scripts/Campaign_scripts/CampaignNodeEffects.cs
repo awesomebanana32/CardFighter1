2eb92be37d43bcb697c95abfaa7e576f043d9fa2
Arnav Choudhury <66573183+8coolguy@users.noreply.github.com>
2025-10-12 22:08:29 -0700

    added terrain generation for campaign map

--- Assets/Scripts/Campaign_scripts/CampaignNodeEffects.cs
+++ Assets/Scripts/Campaign_scripts/CampaignNodeEffects.cs
@@ -6,48 +6,51 @@ public class CampaignNodeEffects : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        // 2. Create a Ray
+        // A Ray is a line segment starting from the camera and going 
+        // through the mouse position into the scene.
+        // This variable will store all the information about the 
+        // object that the ray hits.
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        // 4. Perform the Raycast
+        // Physics.Raycast attempts to fire the ray. 
+        // - 'ray' is the starting line.
+        // - 'out hit' means if it hits something, put the info in the 'hit' variable.
+        // - '100f' is the max distance the ray can travel (optional).
+        if (Physics.Raycast(ray, out hit, 500f))
         {
-            // 2. Create a Ray
-            // A Ray is a line segment starting from the camera and going 
-            // through the mouse position into the scene.
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            // 3. Define the Hit Info
-            // This variable will store all the information about the 
-            // object that the ray hits.
-            RaycastHit hit;
-
-            // 4. Perform the Raycast
-            // Physics.Raycast attempts to fire the ray. 
-            // - 'ray' is the starting line.
-            // - 'out hit' means if it hits something, put the info in the 'hit' variable.
-            // - '100f' is the max distance the ray can travel (optional).
-            if (Physics.Raycast(ray, out hit, 500f))
+            // We successfully hit a 3D object!
+            // 5. Access the Hit Object
+            GameObject clickedObject = hit.collider.gameObject;
+            Renderer renderer = this.GetComponent<Renderer>();
+            if (clickedObject.transform ==this.transform)
             {
-                // We successfully hit a 3D object!
-
-                // 5. Access the Hit Object
-                GameObject clickedObject = hit.collider.gameObject;
-                // Example Action: Change the material color of the clicked object
-                Renderer renderer = clickedObject.GetComponent<Renderer>();
-
-                if (clickedObject.transform == transform)
+                if (GetComponent<Renderer>() != null)
                 {
-                    OnClick();
+                    GetComponent<Renderer>().material.color = Color.gold;
                 }
-                if (renderer != null)
+                if (Input.GetMouseButtonDown(0))
+                {
+                    if (clickedObject.transform == transform)
                     {
-                        renderer.material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+                        OnClick();
                     }
-
-                // You can call a function on the clicked object's script here:
-                // ExampleScript targetScript = clickedObject.GetComponent<ExampleScript>();
-                // if (targetScript != null)
-                // {
-                //     targetScript.TakeDamage(); 
-                // }
+                    if (GetComponent<Renderer>() != null)
+                    {
+                        this.GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+                    }
+                }
             }
+            else
+            {
+                this.GetComponent<Renderer>().material.color = Color.gray;
+            }
+
+        }
+        else
+        {
+                this.GetComponent<Renderer>().material.color = Color.gray;
         }
     }
 
---
+++ Assets/Scripts/Campaign_scripts/NoiseMapGeneration.cs
@@ -0,0 +1,68 @@
+/*
+    NoiseMapGeneration.cs
+    by 8coolguy
+    Code was taken from previous project.
+    https://github.com/8coolguy/pcg-solar-system/blob/master/Assets/Scripts/NoiseMapGeneration.cs
+
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseMapGeneration
+{
+    public static float[,] GenerateNoiseMap(int mapHeight, int mapWidth, float scale, float offsetX, float offsetZ, Wave[] waves)
+    {
+        float[,] noiseMap = new float[mapHeight, mapWidth];
+        for (int i = 0; i < mapHeight; i++)
+        {
+            for (int j = 0; j < mapWidth; j++)
+            {
+                //change the unifrom gen to make it look better in final project
+                float noise = 0.0f;
+                float normal = 0.0f;
+                foreach (Wave wave in waves)
+                {
+                    noise += wave.amplitude * Mathf.PerlinNoise(((i + offsetX) / scale) * (wave.seed + wave.frequency), ((j + offsetZ) / scale) * (wave.seed + wave.frequency));
+                    normal += wave.amplitude;
+                }
+                noise /= normal;
+                noiseMap[i, j] = noise;/*Random.Range(0, 3);*/
+                // Debug.Log(Mathf.PerlinNoise(j / scale, i / scale));
+                // Debug.Log(Mathf.PerlinNoise(j / scale, i / scale));
+            }
+        }
+        return noiseMap;
+    }
+    public static float Perlin3D(float x, float y, float z)
+    {
+        float ab = Mathf.PerlinNoise(x, y);
+        float bc = Mathf.PerlinNoise(y, z);
+        float ac = Mathf.PerlinNoise(x, z);
+
+        float ba = Mathf.PerlinNoise(y, x);
+        float cb = Mathf.PerlinNoise(y, z);
+        float ca = Mathf.PerlinNoise(z, x);
+
+        float abc = ab + bc + ac + ba + cb + ca;
+        return abc / 6f;
+    }
+    public float[,,] Generate3dNoiseMap(int mapx, int mapy, int mapz, float scale, float offsetX,float offsetY,float offsetZ)
+    {
+        float[,,] noiseMap = new float[mapx, mapy, mapz];
+        for (int i=0;i<mapx;i++)
+        {
+            for (int j=0;j<mapy;j++)
+            {
+                for (int k=0;k<mapz;k++)
+                {
+                    float noise=Perlin3D(i,j,k);
+                    noiseMap[i, j, k] = noise;
+
+                }
+            }
+        }
+        return noiseMap;
+    } 
+}
+    
