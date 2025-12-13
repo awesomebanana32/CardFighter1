85f0f784af242034db835094dc95e34da7c84ed7
Ian Wong <102541860+awesomebanana32@users.noreply.github.com>
2025-12-13 10:38:17 -0800

    Fog of War changes
    -Fog revealer radius can now be changed
    -fix bug where troops death will crash game

--- Assets/Scripts/UI/FogOfWar.cs
+++ Assets/Scripts/UI/FogOfWar.cs
@@ -1,16 +1,15 @@
 /*
-* FogOfWar.cs 
-* arnav
-* Covers the map in a r * r grid. Determines which grid to show based on the stealth attribute of the character. 
-* refers to this tutorial with most of the code from this: https://www.youtube.com/watch?v=TjFTgEpPnQg
-*/
+ * FogOfWar.cs
+ * Covers the map in an r x r grid and reveals areas based on vision sources.
+ */
 using UnityEngine;
 using System.Collections.Generic;
-using System;
-using UnityEngine.Rendering.PostProcessing;
+
 public class FogOfWar : MonoBehaviour
 {
-    public FogOfWar Instance;
+    public static FogOfWar Instance;
+
+    [Header("Fog Settings")]
     public int resolution = 32;
     public int worldSize = 100;
     public float fogHeight = 30f;
@@ -21,207 +20,141 @@ public class FogOfWar : MonoBehaviour
     private Color32[] pixels;
     private byte[,] fogData;
     private MeshRenderer fogRenderer;
-    private List<VisionSource> sources = new List<VisionSource>();
+
+    private List<FogOfWarSource> sources = new List<FogOfWarSource>();
 
     void Awake()
     {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
         Instance = this;
     }
 
     void Start()
     {
-        //tiles = new FogOfWarTile[resolution,resolution];
         fogTexture = new Texture2D(resolution, resolution, TextureFormat.RGBA32, false);
         fogTexture.filterMode = FilterMode.Bilinear;
+
         fogData = new byte[resolution, resolution];
-        for(int i = 0; i < resolution; i++)
-            for(int j = 0; j < resolution; j++)
-                fogData[i,j] = 0;
         pixels = new Color32[resolution * resolution];
 
         for (int i = 0; i < pixels.Length; i++)
             pixels[i] = defaultColor;
-        
+
         fogTexture.SetPixels32(pixels);
         fogTexture.Apply();
-        //creates new fogPlace Object
+
         GameObject fogPlane = GameObject.CreatePrimitive(PrimitiveType.Quad);
         fogPlane.name = "FogPlane";
         fogPlane.transform.position = new Vector3(worldSize / 2f, fogHeight, worldSize / 2f);
         fogPlane.transform.localScale = new Vector3(worldSize, worldSize, 1f);
         fogPlane.transform.rotation = Quaternion.Euler(90f, 0f, 0f);
-        //adds the fogMaterial and fogTexture to the Object
+
         fogRenderer = fogPlane.GetComponent<MeshRenderer>();
         fogRenderer.material = fogMaterial;
         fogRenderer.material.mainTexture = fogTexture;
-        //Not sure what yet this does
-        Vector2 texelSize = new Vector2(1f / resolution, 1f / resolution);
-        fogMaterial.SetVector("_TexelSize", new Vector4(texelSize.x, texelSize.y, 0, 0));
 
-        // Apply the fog update immediately
-        UpdateFog();
-        
+        UpdateTexelSize();
     }
 
-    // Update is called once per frame
     void LateUpdate()
     {
         ClearFogData();
-        UpdateFog();        
+
+        foreach (var source in sources)
+        {
+            if (source == null) continue;
+
+            RevealCircle(
+                WorldToTex(source.transform.position),
+                source.stealthRadius
+            );
+        }
+
+        UpdateFogTexture();
     }
+
     void ClearFogData()
     {
-        for (int i = 0; i < resolution; i++)
-        {
-            for (int j = 0; j < resolution; j++)
-            {
-                fogData[i,j] = 0;
-            }
-        }
-
+        for (int x = 0; x < resolution; x++)
+            for (int y = 0; y < resolution; y++)
+                fogData[x, y] = 0;
     }
 
-    void UpdateFog()
+    void UpdateFogTexture()
     {
-        foreach(VisionSource v in sources)
+        for (int x = 0; x < resolution; x++)
         {
-            RevealCircle(WorldToTex(v.transform.position),v.stealthRadius);
-        }
-        for (int i = 0; i < resolution; i++)
-        {
-            for (int j = 0; j < resolution; j++)
+            for (int y = 0; y < resolution; y++)
             {
-                int index = j * resolution + i;
-                byte state = fogData[i, j];
-
-                if (state == 0)
-                    pixels[index] = defaultColor;
-                else if (state == 1)
-                    //pixels[index] = Color.gray; // Optional make grey pixels
-                    pixels[index] = Color.clear;
-                else
-                    pixels[index] = Color.clear;
+                int index = y * resolution + x;
+                pixels[index] = fogData[x, y] == 0 ? defaultColor : Color.clear;
             }
         }
 
         fogTexture.SetPixels32(pixels);
         fogTexture.Apply();
-
-        Vector2 texelSize = new Vector2(1f / resolution, 1f / resolution);
-        fogMaterial.SetVector("_TexelSize", new Vector4(texelSize.x, texelSize.y, 0, 0));
-
+        UpdateTexelSize();
     }
+
+    void UpdateTexelSize()
+    {
+        Vector2 texelSize = new Vector2(1f / resolution, 1f / resolution);
+        fogMaterial.SetVector("_TexelSize",
+            new Vector4(texelSize.x, texelSize.y, 0, 0));
+    }
+
     void RevealCircle(Vector2Int center, float radiusWorld)
     {
-        // Convert the radius from world units to texture pixels
         int radiusTex = Mathf.RoundToInt((radiusWorld / worldSize) * resolution);
 
-        // Get the x and y coordinate of the circle center in texture space
-        int cx = center.x;
-        int cy = center.y;
-
-        // Loop over a square region that bounds the circle (from -radiusTex to +radiusTex)
         for (int y = -radiusTex; y <= radiusTex; y++)
         {
             for (int x = -radiusTex; x <= radiusTex; x++)
             {
-                // Calculate the actual pixel coordinates in the texture
-                int fx = cx + x;
-                int fy = cy + y;
+                int fx = center.x + x;
+                int fy = center.y + y;
 
-                // Skip if the pixel is outside the texture boundaries
-                if (fx < 0 || fx >= resolution || fy < 0 || fy >= resolution) continue;
-  
+                if (fx < 0 || fx >= resolution || fy < 0 || fy >= resolution)
+                    continue;
+
                 if (x * x + y * y <= radiusTex * radiusTex)
-                {
-                    // Mark this pixel as currently visible (2) in the fogData array
-                    fogData[fx, fy] = 2; // Visible
-                }
+                    fogData[fx, fy] = 1;
             }
         }
-    }
-    //Make FogOfWar a singleton
-    public void RegisterVisionSource(VisionSource v)
-    {
-        if(!sources.Contains(v))
-            sources.Add(v);
-    }
-
-    public void UnRegisterVisionSource(VisionSource v)
-    {
-        Debug.Log(" Unregistering " + v.transform.gameObject.name);
-        if(sources.Contains(v))
-            sources.Remove(v);
-    }
-
-    void OnDrawGizmosSelected()
-    {
-        if(fogRenderer == null)
-            return;
-        Gizmos.color = Color.green;
-        Vector3 size = new Vector3(worldSize, 0.1f, worldSize);
-        Vector3 fog = fogRenderer.transform.position;
-        Gizmos.DrawWireCube(fog, size);
     }
 
     Vector2Int WorldToTex(Vector3 worldPos)
     {
         float normX = Mathf.InverseLerp(0, worldSize, worldPos.x);
         float normZ = Mathf.InverseLerp(0, worldSize, worldPos.z);
-        int x = Mathf.FloorToInt(normX * resolution);
-        int y = Mathf.FloorToInt(normZ * resolution);
-        return new Vector2Int(x,y);
+
+        int x = Mathf.Clamp(Mathf.FloorToInt(normX * resolution), 0, resolution - 1);
+        int y = Mathf.Clamp(Mathf.FloorToInt(normZ * resolution), 0, resolution - 1);
+
+        return new Vector2Int(x, y);
     }
 
-    void printFogData()
+    public void RegisterVisionSource(FogOfWarSource source)
     {
-        String output = "";
-        for(int i = 0; i < resolution; i++)
-        {
-            for(int j = 0; j < resolution; j++)
-            {
-               output+=fogData[i,j];
-            }
-            output+="\n";
-        }
-    }
-}
-public class VisionSource
-{
-    //TODO: Figure out why transforms get deleted after about a minute
-    public float stealthRadius;
-    public Transform transform;
-    public VisionSource(Transform t, float s)
-    {
-        stealthRadius = s;
-        transform = t;
-    }
-}
-
-public class FogOfWarTile : MonoBehaviour
-{
-    private bool shown = false;
-    private GameObject parent;
-
-    void Update()
-    {
-        if (!shown)
-        {
-           Hide();
-        }
-        else
-        {
-            UnHide();
-        }
+        if (!sources.Contains(source))
+            sources.Add(source);
     }
 
-    void Hide()
+    public void UnRegisterVisionSource(FogOfWarSource source)
     {
-        //Do operation to make parent gameObject cover the map tile
-    }
-    void UnHide()
-    {
-        //Do operation to show the map, usually just disable
+        sources.Remove(source);
     }
 
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(
+            new Vector3(worldSize / 2f, fogHeight, worldSize / 2f),
+            new Vector3(worldSize, 0.1f, worldSize)
+        );
+    }
 }
--- Assets/Scripts/UI/FogOfWarSource.cs
+++ Assets/Scripts/UI/FogOfWarSource.cs
@@ -1,24 +1,38 @@
 using UnityEngine;
+using System.Collections;
 
 public class FogOfWarSource : MonoBehaviour
 {
-    private VisionSource vs;
-    void Start()
+    [Header("Vision")]
+    public float stealthRadius = 5f;
+
+    private bool registered = false;
+
+    void OnEnable()
     {
-        vs = new VisionSource(this.transform,5f);
-        //TODO: Look up the steath radius from ScriptableObject
-        //TODO: Turn the Fog into a singleton for the scene
-        GameObject fog = GameObject.Find("Fog");
-        if(fog != null)
-            fog.GetComponent<FogOfWar>().RegisterVisionSource(vs);
+        StartCoroutine(TryRegister());
     }
 
-    void OnDestroy()
+    IEnumerator TryRegister()
     {
-        Debug.Log("A Fog Source had been destroyed");
-        GameObject fog = GameObject.Find("Fog");
-        if(fog != null)
-            fog.GetComponent<FogOfWar>().UnRegisterVisionSource(vs);
-        Start();
+        while (!registered)
+        {
+            if (FogOfWar.Instance != null)
+            {
+                FogOfWar.Instance.RegisterVisionSource(this);
+                registered = true;
+                yield break;
+            }
+            yield return null;
+        }
+    }
+
+    void OnDisable()
+    {
+        if (registered && FogOfWar.Instance != null)
+        {
+            FogOfWar.Instance.UnRegisterVisionSource(this);
+            registered = false;
+        }
     }
 }
